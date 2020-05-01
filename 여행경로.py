3eb6f2ce6d7e06ccea691cb372468f4c615c85e4
green-elaia <pharos.veritatis@gmail.com>
2020-05-01 19:31:57 +0900

    200501

--- 여행경로.py
+++ 여행경로.py
@@ -19,3 +19,24 @@ def solution(tickets):
             dest = visited.pop()
             used_ticket[[visited[-1], dest]] = 0
     return visited
+
+
+
+def solution(tickets):
+    # 사전을 이용하여 각 공항에서 출발하는 항공권의 리스트를 표현하고 리스트는 역순으로 정렬. 그래야 시간 덜 걸림
+    # 그래프를 구현하는 방식은 정방행렬을 이용한 인접행렬 방식과 연결리스트를 이용한 인접리스트 방식이 있음
+    routes = {}   # 문제의 그래프를 딕셔너리로 구현(인접리스트 방식과 비슷)
+    for t in tickets:
+        routes[t[0]] = routes.get(t[0], []) + [t[1]]
+    for r in routes:
+        routes[r].sort(reverse=True)
+    stack = ['ICN']
+    path = []
+    while len(stack) > 0:
+        top = stack[-1]
+        if top not in routes or len(routes[top]) == 0:  # 한붓그리기 특성 때문에 가능한 조건
+            path.append(stack.pop())
+        else:
+            stack.append(routes[top][-1])
+            routes[top] = routes[top][:-1]
+    return path[::-1]
